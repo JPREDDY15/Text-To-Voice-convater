1de1b9201e14d81982672dcf08c8b695326fef08
Janga Prashanth Reddy <161188052+JPREDDY15@users.noreply.github.com>
2024-10-20 21:30:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
-let speech=new SpeechSynthesisUtterance();
-let voices=[];
-let voiceSelect=document.querySelector("select");
-window.speechSynthesis.onvoiceschanged=()=>{
-    voices=window.speechSynthesis.getVoices();
-    speech.voice=voices[0];
-    voices.forEach((voice,i)=>(voiceSelect.options[i]=new Option(voice.name,i)));
+let speech = new SpeechSynthesisUtterance();
+let voices = [];
+let voiceSelect = document.querySelector("select");
+
+window.speechSynthesis.onvoiceschanged = () => {
+    voices = window.speechSynthesis.getVoices();
+    speech.voice = voices[0]; // Set the default voice
+    voiceSelect.innerHTML = ''; // Clear previous options
+    voices.forEach((voice, i) => {
+        const option = new Option(voice.name + ' (' + voice.lang + ')', i);
+        voiceSelect.add(option);
+    });
 };
-voiceSelect.addEventListener("change",()=>{
-    speech.voice=voices[voiceSelect.value];
+
+voiceSelect.addEventListener("change", () => {
+    speech.voice = voices[voiceSelect.value];
 });
-document.querySelector("button").addEventListener("click",()=>{
-    speech.text=document.querySelector("textarea").value;
-    window.speechSynthesis.speak(speech);
-})
+
+document.querySelector("button").addEventListener("click", () => {
+    const text = document.querySelector("textarea").value.trim();
+    if (text) {
+        speech.text = text;
+        window.speechSynthesis.speak(speech);
+    } else {
+        alert("Please enter some text to convert.");
+    }
+});
